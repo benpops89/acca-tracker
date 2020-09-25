6ec33cb119986b7c66f62b2cc7de6fb62abd214a
Ben Poppy <ben.poppy78@gmail.com>
2020-09-25 11:39:20 +0100

    Proxy pass graphql requests to docker container

--- acca-tracker/db.py
+++ acca-tracker/db.py
@@ -1,12 +1,10 @@
 import os
 from neo4j import GraphDatabase
 
-HOST = os.environ['NEO4J_HOST']
-
 def connect(func):
     def func_wrapper(*args, **kwargs):
         driver = GraphDatabase.driver(
-            f'neo4j://{HOST}',
+            f'neo4j://neo4j:7687',
             auth=('neo4j', 'SmellyRhino@89')
         )
         return func(driver, *args, **kwargs)
--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ pip3 install -r $HOME/projects/acca-tracker/acca-tracker/requirements.txt
 # Create bin folder
 mkdir $HOME/bin
 export PATH="$PATH:$HOME/bin"
-export NEO4J_HOST="localhost:7687"
 
 # Make cli executeable
 chmod +x $HOME/projects/acca-tracker/acca-tracker/cli.py
